15876d6df97a0abeb8bbd52600234388a25ffab2
Shashy Dass <ShashyD@gmail.com>
2023-04-04 14:32:34 -0400

    Modify jext config and dependencies for imports in test.

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: 'miniflare',
   testMatch: ['**/*.test.ts', '**/test/*.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'esbuild-jest',
+  },
 };
--- apps/api/test/api.test.ts
+++ apps/api/test/api.test.ts
@@ -1,3 +1,7 @@
+import api from '../src/index';
+
+const env = getMiniflareBindings();
+
 describe('A first test', () => {
   test('A named test', () => {
     expect(1).toBe(1);
