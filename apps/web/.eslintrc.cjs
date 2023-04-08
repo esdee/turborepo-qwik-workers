4a7c9e63b046608813a4b3ae5858a1303034baa5
Shashy Dass <ShashyD@gmail.com>
2023-04-08 16:02:01 -0400

    Simplify actions, add tests for Web

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -21,6 +21,7 @@ module.exports = {
     },
   },
   plugins: ['@typescript-eslint'],
+  ignorePatterns: ['**/*.test.ts', '**/test/*.test.ts'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
---
+++ apps/web/test/first-test.test.ts
@@ -0,0 +1,5 @@
+describe('A fake test', () => {
+  test('It will always pass', () => {
+    expect(true).toBe(true);
+  });
+});
